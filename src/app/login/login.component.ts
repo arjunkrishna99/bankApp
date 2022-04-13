aa1065cc1a234e7c4014bee2a685c2c2cfd01a36
Arjun <arjunscms@gmail.com>
2022-04-13 17:19:14 +0530

    one way binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  accNum="account number please!"
+  acno=""
+  pswd=""
+  // database
+  database = {
+     1000 : {acno : 1000,uname:"arjun",password:1000,balance:5000},
+     1001 : {acno : 1001,uname:"arjun",password:1000,balance:6000},
+     1002 : {acno : 1002,uname:"arjun",password:1000,balance:7000}
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+  login(){
+    var acno = this.acno
+    var pswd = this.pswd
+    let database=this.database
+    if(acno in database){
+      if(pswd==database[acno]["password"])
+
+    }
+    else{
+      alert("user does not exist!")
+    }
+  }
+  acnoChange(event:any){
+    this.acno=event.target.value
+  }
+  pswdChange(event:any){
+    this.pswd=event.target.value
   }
 
 }
